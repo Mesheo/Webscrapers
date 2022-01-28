63dfe9115e0d170b0faec6dc871f7ae9ac0a80cb
Michel <m.ledig.mich@gmail.com>
2022-01-28 15:53:35 -0300

    Adding a demo example

--- demo_scrape.py
+++ demo_scrape.py
@@ -1,6 +1,11 @@
 from bs4 import BeautifulSoup
 import requests, json
 
+'''
+This code will display all cryptocurrency names from the website present on the URL
+You can check the XPaths on the developer mode of your browser to find the same information
+'''
+
 response = requests.get(
                 url="https://cryptorank.io/active-ico",
                 headers={
@@ -11,4 +16,4 @@ page_html = response.text
 soup = BeautifulSoup(page_html, 'html.parser')
 
 for tr in soup.find_all("tr", attrs={"class": "styled__WrappedTableRow-sc-13ogi24-3 haOyIW"}):
-    print(tr.div['title']) # Show all the cripto names
+    print(tr.div['title']) 
